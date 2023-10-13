2daba794ef87cee9f452ef06cba960ecbecbcc58
Steven Powell <44844360+spowelljr@users.noreply.github.com>
2023-10-13 11:05:50 -0700

    Update cmd/benchmark.go

--- cmd/benchmark.go
+++ cmd/benchmark.go
@@ -16,9 +16,7 @@ func main() {
 	runs := flag.Int("runs", 100, "number of runs per benchmark")
 	profile := flag.String("profile", "benchmark", "profile to use for minikube commands")
 	images := flag.String("images", "", "a comma separated list of images to benchmark")
-
 	benchFlows := flag.String("iters", "iterative,non-iterative", "a comma separated list of flows to benchmark, options [iterative,non-iterative]")
-
 	benchMethods := flag.String("bench-methods", "", "a comma separated list of benchmark method names")
 	memory := flag.String("memory", "", "Amount of RAM to allocate to Kubernetes (format: <number>[<unit>], where unit = b, k, m or g). Use \"max\" to use the maximum amount of memory")
 
